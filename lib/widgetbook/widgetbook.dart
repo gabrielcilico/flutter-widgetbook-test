37e69a7db70cbe74b1c932f5d9cf1281474c5e3f
cilico <gabrielcilico@gmail.com>
2024-07-24 21:23:03 -0300

    move widgetbook structure

--- lib/widgetbook/widgetbook.dart
+++ lib/widgetbook/widgetbook.dart
@@ -4,11 +4,9 @@ import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 import 'widgetbook.directories.g.dart';
 
-void main() => runApp(const WidgetbookApp());
-
 @widgetbook.App()
-class WidgetbookApp extends StatelessWidget {
-  const WidgetbookApp({super.key});
+class WidgetBookApp extends StatelessWidget {
+  const WidgetBookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/widgetbook_main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widgetbook_test/widgetbook/widgetbook.dart';
+
+void main() => runApp(const WidgetBookApp());
